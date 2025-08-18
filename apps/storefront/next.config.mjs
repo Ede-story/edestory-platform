01594004094ad63591c24d331ade7c5fcf624d62
edestory <288vadim@gmail.com>
2025-08-18 07:37:06 -0700

    chore: add next.config.mjs

---
+++ apps/storefront/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,  experimental: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
